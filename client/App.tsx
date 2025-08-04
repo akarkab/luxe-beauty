60abda06e1022e82ee2f5e196deca27499490bd3
Akarkab Abdelghani <akarkab.abdelghani.dev@gmail.com>
2025-08-04 13:26:36 +0100

    Update App.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/luxe-beauty">
         <Layout>
           <Routes>
             <Route path="/" element={<Index />} />
